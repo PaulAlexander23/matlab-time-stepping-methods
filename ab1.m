57d8d45c913af04ef7ce80fc9da6c9995d103280
Paul Alexander <paulalexander23@live.co.uk>
2020-03-03 11:18:11 +0000

    Correct dimension size in ab1.

--- ab1.m
+++ ab1.m
@@ -2,7 +2,7 @@ function y = ab1(odefun,t,y0)
     n = length(t);
     y = zeros(length(y0),n);
     
-    y(:,:,1) = y0;
+    y(:,1) = y0;
     
     for i = 2:n
         y(:,i) = y(:,i-1) + (t(i)-t(i-1)) * odefun(t(i-1), y(:,i-1));
